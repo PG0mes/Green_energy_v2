8bbd73b464183196de5abde778905cf5128e808b
Pedro Gomes <p.gomesrobertesilva@gmail.com>
2025-10-20 15:02:28 -0300

    Implementação do Tailwind e correção das telas

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/templates/**/*.html" // Adicione esta linha
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
